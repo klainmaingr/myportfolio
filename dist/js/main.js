3bd85c7134903e0ed9cb55eb00331d7335575eee
George Kountardas <klainmaingr@gmail.com>
2018-11-13 20:14:21 +0100

    Homepage overlay and menu items

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-//Select items
+//Select items from the index.html with queryselector
 
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
@@ -6,11 +6,14 @@ const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
-//Initial state of menu
+//Initial state of menu (not pressed)
 
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
+//function add/removes pseudoclass to elements when event happens (click in that case). Then we can use those to add css attributes.
+//ie menu -> menu.show (&.show)
 
 function toggleMenu() {
   if (!showMenu) {
